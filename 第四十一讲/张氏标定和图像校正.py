d5b9cec1b51dc6c9ab3a2d91a770e05f691e8615
lcl1026504480 <17863130570@163.com>
2019-01-22 18:54:55 +0800

    加上了校正的代码

--- 第四十一讲/张氏标定和图像校正.py
+++ 第四十一讲/张氏标定和图像校正.py
@@ -10,7 +10,6 @@ op = []
 imgpoints = []
 for i in os.listdir(path):
     file = '/'.join((path, i))
-    print(file)
     a = cv2.imread(file)
     b = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
     ret, corners = cv2.findChessboardCorners(a, (6, 4), None)
@@ -20,16 +19,30 @@ for i in os.listdir(path):
         imgpoints.append(corners2)
         op.append(objp)
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(op, imgpoints, b.shape[::-1], None, None)
-print(ret)
-print(mtx)
-print(len(rvecs), rvecs[0])
-print(len(tvecs), rvecs[0])
 tot_error = 0
 for i in range(len(op)):
     imgpoints2, _ = cv2.projectPoints(op[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)
+    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
     tot_error += error
-print(tot_error / 24)
+# print(ret, (tot_error / len(op))**0.5)
 # cv2.namedWindow('winname', cv2.WINDOW_NORMAL)
 # cv2.imshow('winname', a)
 # cv2.waitKey(0)
+"""下面是校正部分"""
+h, w = a.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (h, w), 1)
+dst = cv2.undistort(a, mtx, dist, None)
+# undistort
+mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 4)
+dst = cv2.remap(a, mapx, mapy, cv2.INTER_LINEAR)
+# crop the image
+# x, y, w, h = roi
+# dst = dst[y:y + h, x:x + w]
+print(mapx.shape)
+print(mapy)
+a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+# print(roi)
+plt.subplot(121), plt.imshow(a), plt.title('source')
+plt.subplot(122), plt.imshow(dst), plt.title('undistorted')
+plt.show()
